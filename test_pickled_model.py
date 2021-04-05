2741029de3ea246084e1efdd74ffaabe69c9c2bb
kentlingcampbell <77463196+kentlingcampbell@users.noreply.github.com>
2021-04-04 18:58:15 -0600

    Prediction from pickle

--- test_pickled_model.py
+++ test_pickled_model.py
@@ -1,17 +1,13 @@
 # test_pickled_model.py
 
-newdata = ____ # load your newdata, see sections above...
+from newdata import newdata
+newdata
 
-pipe = ____ # unpickle the pipe...
+import pickle
+with open('model.pkl', 'rb') as f:
+    # The protocol version used is detected automatically, so we do not
+    # have to specify it.
+    pipePKL = pickle.load(f)
 
-# `model.predict` and friends expect a 2-d array of data. If your newdata is
-# one-dimensional, make it two-dimensional by wrapping it in a list.
-# numpy can be used to check the number of dimensions of a list.
-#    
-#    import numpy as np
-#    newdata = np.array(newdata)
-#    if newdata.ndim == 1:
-#        newdata = [newdata]
-
-predictions = pipe.predict_proba(newdata)
+predictions = pipePKL.predict(newdata)
 print(predictions)
