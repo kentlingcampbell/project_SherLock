1c1ca5aa1056b90d2b0aa2810b7b70ab42c36713
kentlingcampbell <77463196+kentlingcampbell@users.noreply.github.com>
2021-04-13 16:54:15 -0600

    Fixed

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import pickle
+
 
 app = Flask(__name__)
 
 # Import model pickle
 with open('model.pkl', 'rb') as f:
-    pipePKL = pickle.load(f)
+    pipe = pickle.load(f)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         the_data = request.get_json(force=True)
         newdata = the_data['newdata']
-        prediction = pipe.predict([newdata])
-        return {'Good day to you. The prediction for the data is' : prediction.tolist()}
+        prediction = pipe.predict(newdata)
+        return jsonify(prediction.tolist())
     else:
         return {'Yo!  Dude!'}
