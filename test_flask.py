b1c873da205aee3be4263bca4c0b925ce62e44c6
kentlingcampbell <77463196+kentlingcampbell@users.noreply.github.com>
2021-04-13 16:55:02 -0600

    Lab, fixed

--- test_flask.py
+++ test_flask.py
@@ -4,10 +4,10 @@ import json
 # POST
 # load the test data
 with open('testdata.json', 'r') as f:
-    testdata = jason.load(f)
+    testdata = json.load(f)
 
 r = requests.post('http://127.0.0.1:5000/predict', json={'newdata': testdata})
 #print(r.json())
 data = r.json()
-prediction = data['prediction']
-print(prediction)
+
+print('Good day to you. The prediction for the data is: ', data)
